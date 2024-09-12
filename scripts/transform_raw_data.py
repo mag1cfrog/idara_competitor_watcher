ca7c0e2a91817202affbdaaf68f4d94d6f2cd766
mag1cfrog <harrywong2017@gmail.com>
2024-09-11 18:30:51 -0700

    Update requirements.txt to include polars and duckdb libraries

--- scripts/transform_raw_data.py
+++ scripts/transform_raw_data.py
@@ -4,7 +4,7 @@ import polars as pl
 def unnest_and_explode(df: pl.DataFrame) -> pl.DataFrame:
     while True:
         struct_cols = [col for col in df.columns if df[col].dtype == pl.Struct]
-        list_struct_cols = [col for col in df.columns if df[col].dtype == pl.List(pl.Struct) and col != 'SalesRankings']
+        list_struct_cols = [col for col in df.columns if df[col].dtype == pl.List(pl.Struct) and (col != 'SalesRankings' and col != 'CompetitivePricing_NumberOfOfferListings')]
         
         if not struct_cols and not list_struct_cols:
             break
