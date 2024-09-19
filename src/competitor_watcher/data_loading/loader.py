3fd937681cd1b1f4a95acdcd06cb2dd4297c8471
mag1cfrog <harrywong2017@gmail.com>
2024-09-19 15:45:31 -0700

    Refactor data_loading/loader.py to include data preparation and schema generation

--- src/competitor_watcher/data_loading/loader.py
+++ src/competitor_watcher/data_loading/loader.py
@@ -5,7 +5,8 @@ from loguru import logger
 import polars as pl
 
 from competitor_watcher.data_loading.retrieve_competitor_info import main as retrieve_competitor_info
-from competitor_watcher.data_loading.transform_raw_data import unnest_and_explode, transform_pl_df_and_generate_schema
+from competitor_watcher.data_loading.transform_raw_data import transform_pl_df_and_generate_schema
+from competitor_watcher.data_loading.data_preparation import prepare_data
 from competitor_watcher.utils.db_management import attach_db
 from competitor_watcher.utils.db_operations import create_table, insert_data
 from competitor_watcher.utils.time_utils import get_current_timestamp
@@ -27,18 +28,10 @@ def data_loader():
     timestamp = get_current_timestamp()
 
     competitor_item_attribute, competitor_item_pricing = retrieve_competitor_info()
-    item_attribute_df = unnest_and_explode(
-          pl.DataFrame(competitor_item_attribute)
-        ).with_columns(
-              pl.lit(timestamp).str.to_datetime().alias('Timestamp')
-            )
+    item_attribute_df, item_pricing_df = prepare_data(timestamp, competitor_item_attribute, competitor_item_pricing)
 
-    item_pricing_df = unnest_and_explode(
-          pl.DataFrame(competitor_item_pricing).unnest('Product').unnest('CompetitivePricing')
-        ).with_columns(
-              pl.lit(timestamp).str.to_datetime().alias('Timestamp')
-            )
     item_attribute_df_2, item_attribute_schema = transform_pl_df_and_generate_schema(item_attribute_df)
+
     item_pricing_df_2, item_pricing_schema = transform_pl_df_and_generate_schema(item_pricing_df)
 
     with duckdb.connect() as conn:
@@ -49,7 +42,6 @@ def data_loader():
     
     logger.info("Data loaded successfully")
 
-    return timestamp
 
 
 if __name__ == "__main__":
