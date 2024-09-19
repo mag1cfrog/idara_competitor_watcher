074627a15d6c3657e6b70330cfdff55b30a88bb8
mag1cfrog <harrywong2017@gmail.com>
2024-09-19 16:49:57 -0700

    Refactor retrieve_inventory.py to retrieve inventory summary and generate schema
    This commit refactors the retrieve_inventory.py file to include the retrieval of inventory summary data and the generation of a schema. It also adds a new import statement for the transform_raw_data module and the config_loader module from the utils package. The code now loads the environment variables using the load_dotenv function and sets the ENV_DISABLE_DONATION_MSG variable to '1'. The inventory summary data is retrieved using the Inventories API client and the result is transformed into a Polars DataFrame. The config file is loaded and a set of ASINs is created from the competitor_asin_dict. The DataFrame is filtered to include only the ASINs from the set and the rows with a non-null lastUpdatedTime. The DataFrame is then sorted by lastUpdatedTime in descending order and only the unique rows for each ASIN are kept. Finally, the transformed DataFrame is returned.

--- src/competitor_watcher/data_loading/retrieve_inventory.py
+++ src/competitor_watcher/data_loading/retrieve_inventory.py
@@ -4,12 +4,48 @@ from dotenv import load_dotenv
 from sp_api.api import Inventories
 import polars as pl
 
+from competitor_watcher.data_loading.transform_raw_data import transform_pl_df_and_generate_schema
+from competitor_watcher.utils import config_loader
+
 
 def get_inventory(credentials: dict):
-    credentials=dict(
-                refresh_token=os.getenv('sp_api_refresh_token'),
-                lwa_app_id=os.getenv('sp_api_lwa_app_id'),
-                lwa_client_secret=os.getenv('sp_api_lwa_client_secret'),
-            )
-
+    load_dotenv()
     os.environ['ENV_DISABLE_DONATION_MSG'] = '1'
+
+    inventory_client = Inventories(
+        credentials=credentials
+    )
+
+    res = inventory_client.get_inventory_summary_marketplace(**{
+        'details': True,
+    }
+    )
+
+    df = pl.DataFrame(res()).unnest(
+        "granularity", "inventorySummaries"
+        ).unnest(
+            "inventoryDetails"
+            ).unnest(
+                "reservedQuantity", "researchingQuantity", "unfulfillableQuantity", "futureSupplyQuantity"
+                )
+    
+
+
+    config = config_loader.load_config()
+
+    own_asin_list = set(asin for asin in config['competitor_asin_dict'].keys())
+
+    # Cast lastUpdatedTime to datetime
+    df = df.with_columns(
+        pl.col("lastUpdatedTime").str.to_datetime(strict=False)
+        )
+
+    df = df.filter(
+        (pl.col("asin").is_in(own_asin_list)) & 
+        (pl.col("lastUpdatedTime").is_not_null())
+        )
+    
+    # For each ASIN, should only have one row with the most recent lastUpdatedTime
+    df = df.sort("lastUpdatedTime", descending=True).unique(subset=["asin"], keep="first")
+
+    return transform_pl_df_and_generate_schema(df)
