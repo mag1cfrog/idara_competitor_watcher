c3881e4c9302b9d6dc9f581c24181e7df4a447dd
mag1cfrog <harrywong2017@gmail.com>
2024-09-18 19:21:06 -0700

    Refactor db_management.py to support different storage types and attach databases
    This commit refactors the `db_management.py` file to support different storage types and attach databases accordingly. It introduces a new `attach_db` function that checks the `STORAGE_TYPE` environment variable and attaches the appropriate database. If the storage type is set to 'POSTGRES', it sets up a connection to a Postgres database and attaches it to DuckDB. Otherwise, it attaches a local DuckDB database using the `STORAGE_DIRECTORY` environment variable. The commit also includes a new `setup_postgre` function that installs and loads the Postgres plugin and sets up the connection to the Postgres database.

--- src/competitor_watcher/utils/db_management.py
+++ src/competitor_watcher/utils/db_management.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 import duckdb
 from loguru import logger
 from dotenv import load_dotenv
@@ -7,7 +9,27 @@ from dotenv import load_dotenv
 def attach_db(conn: duckdb.DuckDBPyConnection) -> duckdb.DuckDBPyConnection:
     load_dotenv()
 
-    sql_query_attach_db = f"""
+    storage_type = os.getenv('STORAGE_TYPE')
+
+    if storage_type == 'POSTGRES':
+        conn = setup_postgre(conn)
+        logger.info("Attaching Postgres database to DuckDB")
+        conn.execute("ATTACH '' AS db (TYPE POSTGRES, SECRET postgres_secret_one);")
+    
+    else:
+        # Local DuckDB Storage
+        logger.info("Attaching local database to DuckDB")
+        storage_directory = Path(os.getenv('STORAGE_DIRECTORY')).resolve()
+        storage_directory.mkdir(parents=True, exist_ok=True)
+        storage_file = storage_directory / "storage.duckdb"
+        conn.execute(f"ATTACH '{str(storage_file)}' AS db ;")
+    
+    return conn
+
+
+def setup_postgre(conn: duckdb.DuckDBPyConnection) -> duckdb.DuckDBPyConnection:
+
+    sql_query_setup_postgre = f"""
     INSTALL postgres;
     LOAD postgres;
 
@@ -19,12 +41,10 @@ def attach_db(conn: duckdb.DuckDBPyConnection) -> duckdb.DuckDBPyConnection:
         USER 'postgres',
         PASSWORD '{os.getenv('POSTGRES_PASSWORD')}'
     );
-
-    ATTACH '' AS db (TYPE POSTGRES, SECRET postgres_secret_one);
-
     """
-    logger.info("Attaching Postgres database to DuckDB")
-    
-    conn.execute(sql_query_attach_db)
-    
+
+    conn.execute(sql_query_setup_postgre)
+
     return conn
+
+
