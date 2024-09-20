1d6950dec741ac0d53d2ff912b494999423f115d
mag1cfrog <harrywong2017@gmail.com>
2024-09-19 17:06:44 -0700

    Refactor data_preparation.py to include retrieving competitor item attribute and pricing data, as well as retrieving inventory data using credentials

--- src/competitor_watcher/data_loading/data_preparation.py
+++ src/competitor_watcher/data_loading/data_preparation.py
@@ -4,10 +4,14 @@ import polars as pl
 from competitor_watcher.data_loading.transform_raw_data import unnest_and_explode
 from competitor_watcher.data_loading.retrieve_competitor_info import retrieve_competitor_info
 from competitor_watcher.data_loading.transform_raw_data import transform_pl_df_and_generate_schema
+from competitor_watcher.data_loading.retrieve_inventory import retrieve_inventory
+from competitor_watcher.utils.load_credential import load_credentials
 
 def prepare_data(timestamp):
 
-    competitor_item_attribute, competitor_item_pricing = retrieve_competitor_info()
+    credentials = load_credentials()
+
+    competitor_item_attribute, competitor_item_pricing = retrieve_competitor_info(credentials)
 
 
     item_attribute_df = unnest_and_explode(
@@ -25,6 +29,8 @@ def prepare_data(timestamp):
     item_attribute_df_2, item_attribute_schema = transform_pl_df_and_generate_schema(item_attribute_df)
 
     item_pricing_df_2, item_pricing_schema = transform_pl_df_and_generate_schema(item_pricing_df)
+
+    inventory_df, inventory_schema = retrieve_inventory(credentials)
 
     return (
             (item_attribute_df_2, item_attribute_schema), 
